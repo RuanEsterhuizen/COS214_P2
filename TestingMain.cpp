52c557c4186941e66cee09caaa95519fc5f341db
YouBeYou <ruben.gadd@outlook.com>
2024-08-11 17:34:01 +0200

    Minor

--- TestingMain.cpp
+++ TestingMain.cpp
@@ -11,12 +11,14 @@ using namespace std;
 
 int main() {
     Soldiers * romeArmy1;
-    ShieldBearer * romeArmy2 = new ShieldBearer(100,100,100,100, "Pretorians");
+    ShieldBearer * romeArmy2 = new ShieldBearer(100,100,100,100, "Principe");
     Soldiers * romeArmy3;
+    Soldiers * romeArmy4;
     InfantryFactory * r1 = new InfantryFactory();
     ShieldBearerFactory * r2 = new ShieldBearerFactory();
     BoatmanFactory * r3 = new BoatmanFactory();
     romeArmy1 = r1->createUnit();
+    romeArmy4 = r2->createUnit();
     romeArmy3 = r3->createUnit();
     romeArmy1->setUnitName("Rome Army 1");
     Caretaker soreLoser;
@@ -30,9 +32,14 @@ int main() {
     cout << "\tTotal Health: " << r1->calculateTotalHealthPerUnit(romeArmy1) << endl << endl;
 
     cout << "Statistical Report of Roman Army 2:\n";
-    cout << "\tTotal Damage: " << r1->calculateTotalDamagePerUnit(romeArmy2) << endl;
-    cout << "\tTotal Defence: " << r1->calculateTotalDefencePerUnit(romeArmy2) << endl;
-    cout << "\tTotal Health: " << r1->calculateTotalHealthPerUnit(romeArmy2) << endl << endl;
+    cout << "\tTotal Damage: " << r2->calculateTotalDamagePerUnit(romeArmy2) << endl;
+    cout << "\tTotal Defence: " << r2->calculateTotalDefencePerUnit(romeArmy2) << endl;
+    cout << "\tTotal Health: " << r2->calculateTotalHealthPerUnit(romeArmy2) << endl << endl;
+
+    cout << "Statistical Report of Roman Army 3:\n";
+    cout << "\tTotal Damage: " << r3->calculateTotalDamagePerUnit(romeArmy3) << endl;
+    cout << "\tTotal Defence: " << r3->calculateTotalDefencePerUnit(romeArmy3) << endl;
+    cout << "\tTotal Health: " << r3->calculateTotalHealthPerUnit(romeArmy3) << endl << endl;
 
 
     Soldiers * parthiaArmy1;
@@ -58,6 +65,11 @@ int main() {
     cout << endl << "Round over" << endl << endl;
     romeArmy2->engage(parthiaArmy3);
     cout << endl << "Round over" << endl << endl;
+    romeArmy3->engage(parthiaArmy3);
+    cout << endl << "Round over" << endl << endl;
+
+    romeArmy1 = new Infantry(200,200,200,200, "Pretorians");
+    Soldiers * romeArmy5 = romeArmy1->clonis();
 
     romeArmy2->vivificaMemento(soreLoser.retrieveMemento());
     romeArmy2->engage(parthiaArmy3);
